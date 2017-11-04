d897518af9e46f1285848a7c2f840c58d5890157
developerTQW2017 <2144799613@qq.com>
2017-11-04 09:01:47 +0800

    Ajax版成功

--- upload.php
+++ upload.php
@@ -2,13 +2,15 @@
 <head>
 <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
 <title>上传你的File吧</title>
+<script src="jquery-3.2.1.min.js">
+</script>
 </head>
 <body/>
 <form id="uploadform">
 <table>
 <tr><td align="center" colspan="2"><font style="font-size:40px;font-family: 华文彩云;">文件上传</font></td></tr>
 <tr><td>请选择你要上传文件：</td><td><input type="file" name="myfile"/></td></tr>
-<tr><td><input type="submit" value="上传文件"/></td></tr>
+<tr><td><input type="button" value="上传文件" onclick="doUpload()"/></td></tr>
 </table>
 </form>
 
@@ -23,9 +25,9 @@
 </body>
 <script>
 function doUpload() {
-     var formData = new FormData($( "#uploadform"  )[0]);
+     var formData = new FormData($("#uploadform")[0]);
      $.ajax({
-          url: 'uploadpri.php' ,
+          url: 'http://120.76.140.147/fileupload/uploadpri.php' ,
           type: 'POST',
           data: formData,
           async: false,
